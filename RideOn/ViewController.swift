565fdbc5f9d0c864e63f1a52e78d4b77a34fdaea
Karen L Irvine <kirvine@andrew.cmu.edu>
2016-09-27 16:15:49 -0400

    pushing map views

--- RideOn/ViewController.swift
+++ RideOn/ViewController.swift
@@ -16,12 +16,6 @@ class ViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var arrivalField: UITextField!
     @IBOutlet weak var alertField: UITextField!
     
-    var timer = busTimer()
-
-    @IBAction func startTimer() {
-        busTimer.startTimer()
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
